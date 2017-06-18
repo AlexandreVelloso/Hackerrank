f1f35adfa969f201716194314e14c686ad37240a
xande1505 <xande.velloso@gmail.com>
2017-06-18 12:48:57 -0300

    .

--- Python/Introduction/Python If-Else.py
+++ Python/Introduction/Python If-Else.py
@@ -8,8 +8,19 @@ if __name__ == '__main__':
         print( "Weird" )
     elif( n >= 2 and n <= 5 ):
         print( "Not Weird" )
-    elif( n >= 6 and n <= 20 ):
+
+    elif( n in range(6,21) ):
         print( "Weird" )
+
     elif( n > 20 ):
         print( "Not Weird" )
 #end main
+
+
+# author: Diapolo10
+'''
+n = int(input().strip())
+check = {True: "Not Weird", False: "Weird"}
+
+print( check[ n%2==0 and ( n in range(2,6) or n > 20) ] )
+'''
