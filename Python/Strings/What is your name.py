b614df5bd40409d01d8732ce322d4f86b25c6d47
xande1505 <xande.velloso@gmail.com>
2017-06-19 16:46:42 -0300

    .

--- Python/Strings/What is your name.py
+++ Python/Strings/What is your name.py
@@ -1,5 +1,9 @@
 def print_full_name(a, b):
     print("Hello {0} {1}! You just delved into python.".format( a, b ) )
+# end print_full_name
+
+# also works
+# print( "Hello %s %s! You just delved into python." % (a,b) )
 
 if __name__ == '__main__':
     first_name = input()
