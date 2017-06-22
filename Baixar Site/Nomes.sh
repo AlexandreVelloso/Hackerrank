ff6c20d918f483ca041467b38d2abe1f14b70477
xande1505 <xande.velloso@gmail.com>
2017-06-22 10:50:21 -0300

    .

--- Baixar Site/Nomes.sh
+++ Baixar Site/Nomes.sh
@@ -1,2 +1,10 @@
-javac *.java ; java Download $1 | grep '</a>' | java Tratar $2
+#menu de ajuda
+if [ $# -eq 0 ];
+then
+  echo "Parametro 1: url da pagina do hackerrank"
+  echo "Parametro 2: extensao do arquivo"
+  echo "Parametro 3: numero da ultima atividade baixada"
+else
+  javac *.java ; java Download $1 | grep '</a>' | java Tratar $2 $3
+fi
 exit
--- Baixar Site/Tratar.java
+++ Baixar Site/Tratar.java
@@ -2,6 +2,41 @@ import java.util.Scanner;
 
 public class Tratar{
 
+  /**
+  * Contar o numero de casas de um numero
+  */
+  public static int contCasas( int num ){
+
+    int cont = 1;
+
+    while( num > 9 ){
+      num /= 10;
+      cont++;
+    }
+
+    return cont;
+  }
+
+  /**
+  * Se eu tenho o numero 20, e quero ele com 3 caracteres
+  * eu tenho que arrumar ele para 020, isso que esse metodo faz
+  */
+  public static String arrumar( int num, int numCasas ){
+    String arrumado = "";
+
+    for (int i = contCasas(num); i < numCasas; i++) {
+      arrumado += "0";
+    }
+
+    arrumado += num;
+
+    return arrumado;
+  }
+
+  /**
+  * Args[0] e' o nome da extensao do arquivo
+  * Args[1] e' o ultimo numero baixado
+  */
   public static void main(String [] args){
 
     Scanner sc = new Scanner( System.in );
@@ -10,6 +45,10 @@ public class Tratar{
     for ( int i = 0; i < 14; i++) {
       sc.nextLine();
     }
+
+    // numero do ultimo exercicio baixado
+    int num = Integer.parseInt( args[1] );
+    int numCasas = args[1].length();
 
     String str;
     while( sc.hasNext() ){
@@ -21,7 +60,10 @@ public class Tratar{
         if( str.equals("1") )
           break;
 
-        System.out.println( "touch "+str.replace(" ","\\ ")+"."+args[0] );
+
+          String name = str.replace(" ","\\ ").replace("!","\\!").replace("(","\\(").replace(")","\\)");
+
+        System.out.printf( "touch \\#%s\\ %s.%s\n", arrumar(++num, numCasas), name, args[0] );
 
 
       }
