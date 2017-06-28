5eda18a81e2033187e306e6cfbc5b9d877f8a854
xande1505 <xande.velloso@gmail.com>
2017-06-28 09:15:58 -0300

    .

--- Python/Basic Data Types/#04 Nested Lists.py
+++ Python/Basic Data Types/#04 Nested Lists.py
@@ -1,1 +1,20 @@
+if __name__ == '__main__':
 
+    students = list()
+    grades = set()
+
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+
+        grades.add( score )
+        students.append( (name,score) )
+    # end for
+
+    grades = list( grades )
+    grades.sort()
+
+    # sort by name
+    students.sort( key = lambda x: x[0] )
+
+    print( *[ x[0] for x in students if( x[1] == grades[1] ) ], sep = '\n')
