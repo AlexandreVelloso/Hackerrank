6ce50d0e82064534db39a0988f1ffa0f82ca12e1
xande1505 <xande.velloso@gmail.com>
2017-07-12 16:15:52 -0300

    contAcimaDir e contDir implentados

--- Algorithms/Implementation/#038 Queen's Attack II.go
+++ Algorithms/Implementation/#038 Queen's Attack II.go
@@ -13,8 +13,10 @@ var posQueen position;
 var obstacles []position;
 // size of the chessboard
 var size int;
-// max int
+// min int
 var MIN = math.MinInt32
+// max int
+var MAX = math.MaxInt32
 
 func mod( a, b int ) int{
   var mod = a-b;
@@ -43,7 +45,7 @@ func isAcimaDiagonalPrincipal( pos position ) bool{
 }
 
 func isAbaixoDiagonalSecundaria( pos position )bool{
-  return (pos.line + pos.column >= size);
+  return ( (pos.line + pos.column) >= size);
 }
 
 func isAcimaDiagonalSecundaria( pos position )bool{
@@ -55,34 +57,135 @@ func isDiagonalPrincipal( pos position ) bool{
 }
 
 /**
-* Conta quantas livres a rainha tem acima dela
+* Verifica se um obstaculo esta acima da rainha, e se ele e'
+* o mais perto ate entao
 */
-func contCima ( pos position, obstacles []position ) int{
+func isAcima( pos, obstacle, obstaculoMaisPerto position ) bool{
 
-  var posObstacle = position{ MIN , MIN };
+  // testa se o obstaculo esta na mesma coluna que a rainha
+  // testa se o obstaculo e' o mais proximo dela nessa coluna
+  return ( obstacle.column == pos.column &&
+           obstacle.line < obstaculoMaisPerto.line )
+}
+
+/**
+* Conta quantas casas livres a rainha tem acima dela
+*/
+func contAcima( pos position, obstacles []position ) int{
+
+  var obstaculoMaisPerto = position{ MIN , MIN };
 
   // test if have a obstacle
   for i := 0; i < len( obstacles ); i++{
 
-    if( obstacles[i].column == pos.column && obstacles[i].line < posObstacle.line ){
-      fmt.Println("hue")
-      posObstacle = obstacles[i]
+    if( isAcima( pos, obstacles[i], obstaculoMaisPerto) ){
+      obstaculoMaisPerto = obstacles[i]
     }
   }
 
   // if have a obstacle
-  if( posObstacle.line != MIN ){
-    return ( pos.line - posObstacle.line )
+  if( obstaculoMaisPerto.line != MIN ){
+    return ( pos.line - obstaculoMaisPerto.line )
   }else{
     return ( pos.line )
   }
 }
 
+/**
+* Verifica se um obstaculo esta na diagonal superior da rainha e se
+* esse obstaculo e' o meis perto ate entao
+*/
+func isAcimaDir( pos, obstaculo, obstaculoMaisPerto position ) bool{
+
+  // para saber se um obstaculo esta na mesma diagonal direita
+  // a soma da linha e coluna vao ser uma constante
+  var constante = pos.line + pos.column
+
+  // Verifica se o obstaculo esta na diagonal superior direita da rainha,
+  // e se ele e' o mais perto
+  // e tbm se ele esta na parte de cima, e nao na de baixo
+  return ( constante == (obstaculo.line + obstaculo.column) && obstaculo.line > obstaculoMaisPerto.line && obstaculo.column > pos.column )
+}
+
+/**
+* Conta quantas casas livres a rainha tem para a diagonal
+* superior direita
+*/
+func contAcimaDir( pos position, obstacles []position ) int{
+
+  var obstaculoMaisPerto = position{ MIN , MIN };
+
+  // test if have a obstacle
+  for i := 0; i < len( obstacles ); i++{
+
+    if( isAcimaDir( pos, obstacles[i],obstaculoMaisPerto ) ){
+      obstaculoMaisPerto = obstacles[i]
+    }
+  }
+
+  // if have a obstacle
+  if( obstaculoMaisPerto.line != MIN ){
+
+    return ( pos.column - obstaculoMaisPerto.line )
+
+  }else{
+
+    if( isAbaixoDiagonalSecundaria(pos) ){
+
+      return ( size - 1 - pos.column )
+    }else{
+
+      return ( pos.line )
+    }
+  }
+}// end function contAcimaDir
+
+/**
+* Verifica se um obstaculo esta na direita da rainha e se ele e' o mais
+* perto ate entao
+*/
+func isDir( pos, obstaculo, obstaculoMaisPerto position ) bool{
+
+  // testa se a rainha e o obstaculo estao na mesma linha
+  // testa se a coluna do obstaculo e' maior do que a coluna da rainha
+  // testa se o obstaculo esta mais perto do que o anterior
+  return ( pos.line == obstaculo.line &&
+           pos.column < obstaculo.column &&
+           obstaculo.column < obstaculoMaisPerto.column )
+}
+
+/**
+* Conta quantas casas livres a rainha tem para a direita
+*/
+func contDir( pos position, obstacles []position ) int{
+
+  var obstaculoMaisPerto = position{ MAX , MAX };
+
+  // test if have a obstacle
+  for i := 0; i < len( obstacles ); i++{
+
+    if( isDir( pos, obstacles[i],obstaculoMaisPerto ) ){
+      obstaculoMaisPerto = obstacles[i]
+    }
+  }
+
+  // if have obstacle
+  if( obstaculoMaisPerto.line != MAX ){
+
+    return( obstaculoMaisPerto.column - 1 - pos.column )
+  }else{
+
+    return( size - 1 - pos.column )
+  }
+}// end method contDir
+
 func solve( ){
   fmt.Println( "posQueen: ",posQueen );
   fmt.Println( "Obstaculos: ",obstacles );
 
-  fmt.Println( contCima(posQueen, obstacles) )
+  // fmt.Println( contAima(posQueen, obstacles) )
+  // fmt.Println( contAcimaDir(posQueen, obstacles) )
+  // fmt.Println( contDir(posQueen, obstacles) )
 }
 
 /**
